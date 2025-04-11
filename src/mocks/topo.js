31ef13cc11a91dc4b7cdf55ba787cee01e18b177
vih-silva <149601172+vih-silva@users.noreply.github.com>
2025-04-11 14:22:17 -0300

    sdga

--- src/mocks/topo.js
+++
@@ -1,6 +0,0 @@
-const topo = {
-    boasVindas: "Olá Richardson",
-    legenda: "Encontre os melhores produtores",
-};
-
-export default topo;
--- src/telas/Home/index.js
+++
@@ -1,6 +0,0 @@
-import react from "react";
-import Topo from './componentes/Topo';
-
-export default function Home(){
-    return<Topo />
-}
